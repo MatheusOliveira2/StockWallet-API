70be51bafab24786b63095c8d2871ffcbb71d728
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-01-12 23:02:35 -0300

    Tabela de stocks

--- src/database/migrations/1610502960937-CreateStocks.ts
+++ src/database/migrations/1610502960937-CreateStocks.ts
@@ -1,7 +1,21 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateStocks1610502960937 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'stocks',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+        ],
+      })
+    );
+  }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
 }
