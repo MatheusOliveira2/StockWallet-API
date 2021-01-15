ec7d1abb159e4c0658ff14a2fc1350ccaed54f9b
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-01-14 22:16:28 -0300

    Conexão com o banco PG com docker

--- src/route.ts
+++ src/route.ts
@@ -1,12 +1,5 @@
 import { Request, Response } from 'express';
-import createUser from './services/CreateUser';
 
 export function helloWorld(req: Request, res: Response) {
-  const user = createUser({
-    name: 'matheus',
-    email: 'matheus@mail.com',
-    password: '123456',
-    techs: ['Node', 'React'],
-  });
   return res.json({ message: 'HW' });
 }
